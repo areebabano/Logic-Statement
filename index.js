5a46df989a1f03dae7a601d88aa9216dd9f4d53f
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2024-05-25 00:54:19 +0500

    logic statement's

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+// if else statements 
+let isBiryani = "Tikka Biryani";
+if (isBiryani == "Tikka biryani") {
+    console.log("i love biryani");
+}
+else {
+    console.log("I don't like biryani");
+}
+// else if 
+let game = "Angry Bird";
+game = "PubG";
+game = "freefire";
+game = "Angry Bird";
+if (game == "PubG") {
+    console.log("Winner Winner Chicken Dinner");
+}
+else if (game == "freefire") {
+    console.log("Congratulation You Win!");
+}
+else if (game == "Angry Bird") {
+    console.log("you win!");
+}
+else {
+    console.log("Not a game");
+}
